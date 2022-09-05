f4f83943dc4fc77aef87a6f383dc9202290f0fa6
oumy <toureoumy12@gmail.com>
2022-09-05 13:59:34 +0000

    tp jumia

--- jumia.js
+++ jumia.js
@@ -1,4 +1,5 @@
 
+// cration d'un slider qui permet de d'aficher plusieurs photo chaque 4s
 var slide = new Array("images/photo2jumia.jpg", "images/photo4slider.jpg", "images/photo5slider.gif", "images/photo6slider.jpg","images/photo9slider.jpeg");
 var numero = 0;
 
@@ -12,7 +13,7 @@ function ChangeSlide(sens) {
 }
 setInterval("ChangeSlide(1)", 4000);
 
-
+// defillement des photos en bas aves deux bouttons bntlef et btnright  
   const derniere = document.getElementsByClassName("derniere")[0];
      let bntleft = document.getElementsByClassName("btnleft")[0];
      let btnright =document.getElementsByClassName("btnright")[0];
